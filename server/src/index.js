474c2c238528beb681dce3030964e9e5bb3a6128
Matt Mannion <mattpmannion@gmail.com>
2021-02-03 17:51:05 -0500

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,9 @@
 const { ApolloServer } = require('apollo-server');
+require('dotenv').config();
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
+const isEmail = require('isemail');
 const { createStore } = require('./utils');
 
 const LaunchAPI = require('./datasources/launch');
@@ -10,6 +12,16 @@ const UserAPI = require('./datasources/user');
 const store = createStore();
 
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    // simple auth check on every request
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (!isEmail.validate(email)) return { user: null };
+    // find a user by their email
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
